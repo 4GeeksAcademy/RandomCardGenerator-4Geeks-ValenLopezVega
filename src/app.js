13a9e935d0e56f17a7e1ec663380f4921e16304c
Valentina López Vega <37059478+ValenLopezVega@users.noreply.github.com>
2025-04-09 21:27:01 +0000

    Random card generator finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 import "bootstrap";
 import "./style.css";
 
+function generarCarta() {
+  const topIcon = document.getElementById("top-icon");
+  const number = document.getElementById("number");
+  const bottomIcon = document.getElementById("bottom-icon");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const symbols = ["♦", "♥", "♠", "♣"];
+  const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  // Elegir uno al azar de cada array
+  const randomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+
+  // Asignar los valores a la carta
+  topIcon.innerText = randomSymbol;
+  number.innerText = randomValue;
+  bottomIcon.innerText = randomSymbol;
+
+  // Cambiar el color si es corazón o diamante (son rojos)
+  const isRed = randomSymbol === "♦" || randomSymbol === "♥";
+  const color = isRed ? "red" : "black";
+
+  topIcon.style.color = color;
+  bottomIcon.style.color = color;
+}
+
+window.onload = generarCarta;
